3dc8bf1a9451805557506c30d7411a4e937265f2
Nicholas Jamieson <nicholas@cartant.com>
2019-05-19 16:49:31 +1000

    Inlined an arrow return statement

--- shells/dev/app/Talk/Repo.js
+++ shells/dev/app/Talk/Repo.js
@@ -2,13 +2,11 @@ import React, { Suspense } from 'react';
 import { Issues, IssuesFallback} from './Issues';
 import styles from './App.css';
 
-export const Repo = ({ repo }) => {
-  return (
-    <div className={styles.Repo}>
-      <p className={styles.RepoName}>{repo.name}</p>
-      <Suspense fallback={<IssuesFallback />}>
-        <Issues repo={repo} />
-      </Suspense>
-    </div>
-  );
-};
+export const Repo = ({ repo }) => (
+  <div className={styles.Repo}>
+    <p className={styles.RepoName}>{repo.name}</p>
+    <Suspense fallback={<IssuesFallback />}>
+      <Issues repo={repo} />
+    </Suspense>
+  </div>
+);
